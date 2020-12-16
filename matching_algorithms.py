32fe5e7c4368025b313eede530ee46dfc915201a
CapnSquirrel <azapataa@g.ucla.edu>
2020-12-16 01:09:58 -0800

    BWT in progress

--- matching_algorithms.py
+++ matching_algorithms.py
@@ -1,4 +1,10 @@
-import operator
+# bad implementation memory-wise
+def create_suffix_array(text):
+    text = text + "$"
+    suffixes = [(i, text[i:]) for i in range(len(text))]
+    suffixes.sort(key=lambda kv: kv[1])
+    return suffixes
+
 # 9.6 on online textbook, with many adjustments
 def pattern_matching_with_suffix_array(text_len, pattern, suffix_array):
     text_len += 1 # plus one for $
@@ -36,13 +42,36 @@ def pattern_matching_with_suffix_array(text_len, pattern, suffix_array):
     last = max_index
     return (first, last)
 
-def better_BW_matching():
-    pass
+def better_BW_matching(first_ocurrence, last_column, pattern, count):
+    top = 0
+    bottom = len(last_column) - 1
+    
+    while top <= bottom:
+        if pattern != "":
+            symbol = pattern[-1]
+            pattern = pattern[:len(pattern)-1]
+            if symbol in last_column[top:bottom+1]: # may be off by one
+                symbol_fo = first_ocurrence[symbol]
+                top = symbol_fo + count[top][symbol]
+                bottom = symbol_fo + count[bottom+1][symbol] - 1
+            else:
+                return 0
+        else:
+            return bottom - top + 1
 
-# bad implementation memory-wise
-def create_suffix_array(text):
-    text = text + "$"
-    suffixes = [(i, text[i:]) for i in range(len(text))]
-    suffixes.sort(key=lambda kv: kv[1])
-    return suffixes
+    return 0
+
+# rotate text to the right by n
+def rotate_right(text, n):
+    return text[-n:] + text[:-n]
+
+# Need M(Text) matrix, BWT = last_column. 
+def create_Mtext(text):
+    CRM = []
+
+    for n in range(len(text)):
+        CRM.append(rotate_right(text, n))
+
+    CRM.sort()
+    return CRM
 
--- pattern_matching.py
+++ pattern_matching.py
@@ -3,19 +3,36 @@ import matching_algorithms as ma
 import timeit
 # from sys import getsizeof
 
-genome_size = 150000
-n_reads = 10000 # X
-read_len = 100 # Y
+def test_suffix_array():
+    genome_size = 150000
+    n_reads = 10000 # X
+    read_len = 100 # Y
 
-human_genome = gg.genereate_genome(genome_size)
-reads = gg.generate_reads(n_reads, read_len, human_genome)
+    human_genome = gg.genereate_genome(genome_size)
+    reads = gg.generate_reads(n_reads, read_len, human_genome)
 
-start = timeit.default_timer()
+    start = timeit.default_timer()
 
-suffix_array = ma.create_suffix_array(human_genome)
-for pattern in reads:
-    ma.pattern_matching_with_suffix_array(len(human_genome), pattern, suffix_array)
+    suffix_array = ma.create_suffix_array(human_genome)
+    for pattern in reads:
+        ma.pattern_matching_with_suffix_array(len(human_genome), pattern, suffix_array)
 
-stop = timeit.default_timer()
-print('Time: ', stop - start)  
-# print(getsizeof(suffix_array))
+    stop = timeit.default_timer()
+    print('Time: ', stop - start)  
+    # print(getsizeof(suffix_array))
+
+def test_BWT():
+    genome_size = 5
+    # n_reads = 1
+    # read_len = 3
+
+    human_genome = gg.genereate_genome(genome_size)
+    # reads = gg.generate_reads(n_reads, read_len, human_genome)
+    
+    BWT = ""
+    for line in ma.create_Mtext(human_genome + "$"):
+        BWT = BWT + line[-1]
+        print(line)
+    print(BWT)
+
+test_BWT()
